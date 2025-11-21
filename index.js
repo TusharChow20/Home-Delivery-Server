0a90c99c49446226770dfdecd114ea30f41f0672
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-11-22 00:43:09 +0600

    full setup

--- index.js
+++ index.js
@@ -3,11 +3,49 @@ const cors = require("cors");
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 3000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // app.use(cors());
 // middleware
 app.use(express.json())
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j7xeedk.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const myDB = client.db("siftingHobe");
+    const parcelCollection = myDB.collection("parcels");
+
+    //=================api's================
+    app.get('/parcels',async (req,res)=>{})
+
+    app.post('/parcels',async (req,res)=>{
+        const parcel = req.body;
+        const result = await parcelCollection.insertOne(parcel)
+        res.send(parcel)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
   res.send("Home-delivery");
