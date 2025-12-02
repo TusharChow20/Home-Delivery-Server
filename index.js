546caab175770ff33d55c6fb926e9f6f986cffd6
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-12-02 18:01:44 +0600

    admin role manage

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/users/:id", async (req, res) => {});
+    app.get("/users/:email/role", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      res.send({ role: user?.role || "user" });
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       user.role = "user";
